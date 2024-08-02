9bc63cb01f025910e37144f4178c7ca67590c33c
adityap5 <aditya29pippal@gmail.com>
2024-08-02 20:34:20 +0530

    bug fixed

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/post', require('./routes/post'));
-
+app.get('/',(req,res)=>{
+res.send("Server Is On!!")
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
